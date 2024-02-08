ef6d52ce2554e18f03820183efc54646b6b0db3e
Sylvain Gamel <code@sylvaingamel.fr>
2024-02-08 23:19:35 +0100

    Initial Commit

---
+++ MessageComposeLayout/MessageComposeLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessageComposeLayoutApp.swift
+//  MessageComposeLayout
+//
+//  Created by Sylvain on 08/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MessageComposeLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
